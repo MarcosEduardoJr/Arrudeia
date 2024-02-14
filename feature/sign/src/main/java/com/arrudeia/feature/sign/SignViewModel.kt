105f1876e287a5a30066d9982a85881664222d2e
marcos <marcoseduflautista1@gmail.com>
2024-02-14 15:17:14 -0300

    strings translate to english

--- feature/sign/src/main/java/com/arrudeia/feature/sign/SignViewModel.kt
+++ feature/sign/src/main/java/com/arrudeia/feature/sign/SignViewModel.kt
@@ -4,18 +4,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.arrudeia.core.data.repository.UserDataRepository
-import com.arrudeia.core.model.data.UserNewsResource
-import com.arrudeia.core.ui.NewsFeedUiState
-import com.arrudeia.core.ui.NewsFeedUiState.Loading
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onStart
-import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
