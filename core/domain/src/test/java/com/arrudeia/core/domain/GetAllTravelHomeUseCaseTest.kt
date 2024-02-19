d0ca13111020173dc7a69537153b10277a136348
marcos <marcoseduflautista1@gmail.com>
2024-02-18 23:56:17 -0300

    unit test GetAllTravelHomeUseCaseTest

---
+++ core/domain/src/test/java/com/arrudeia/core/domain/GetAllTravelHomeUseCaseTest.kt
@@ -0,0 +1,29 @@
+package com.arrudeia.core.domain
+
+import com.arrudeia.core.data.network.DefaultHomeTravelsRepositoryImpl
+import com.arrudeia.core.data.network.StoriesRepositoryImpl
+import com.arrudeia.core.data.repository.TravelRepositoryEntity
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import kotlin.test.assertTrue
+
+class GetAllTravelHomeUseCaseTest {
+    private lateinit var repository: DefaultHomeTravelsRepositoryImpl
+    private lateinit var useCase: GetAllTravelHomeUseCase
+
+    @Before
+    fun setup() {
+        repository = Mockito.mock()
+        useCase = GetAllTravelHomeUseCase(repository)
+    }
+
+    @Test
+    fun `invoke use case is not empty`() = runBlocking {
+        val listResult = mutableListOf(TravelRepositoryEntity())
+        `when`(repository.getAllTravels()).thenReturn(listResult)
+        assertTrue(useCase.invoke().isNotEmpty())
+    }
+}
