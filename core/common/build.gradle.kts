3bf2c2c55c7dfb039089758846eb7e0b7c6a7b07
MarcosEduardoJr <marcoseduflautista1@gmail.com>
2024-07-19 12:56:44 -0300

    fix bitrise

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -30,7 +30,7 @@ excludes.add("META-INF/LICENSE-notice.md")
         }
     }
     compileOptions {
-        isCoreLibraryDesugaringEnabled = true
+        isCoreLibraryDesugaringEnabled = false
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
