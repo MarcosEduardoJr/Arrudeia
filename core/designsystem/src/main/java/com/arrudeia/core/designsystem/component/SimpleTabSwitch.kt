6875e92333f66be219af97fb91bfed42159319f8
MarcosEduardoJr <marcoseduflautista1@gmail.com>
2024-10-08 00:48:42 -0300

    change layout swtch tab radius

--- core/designsystem/src/main/java/com/arrudeia/core/designsystem/component/SimpleTabSwitch.kt
+++ core/designsystem/src/main/java/com/arrudeia/core/designsystem/component/SimpleTabSwitch.kt
@@ -1,19 +1,22 @@
 package com.arrudeia.core.designsystem.component
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
-import androidx.compose.material3.TabRowDefaults
-import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -33,6 +36,7 @@ fun SimpleTabSwitch(
     val coroutineScope = rememberCoroutineScope()
     val colorPrimary = colorResource(id = colorPrimary)
     val textGrey = colorResource(id = text_grey)
+    val interactionSource = remember { MutableInteractionSource() }
     Column {
         TabRow(
             selectedTabIndex = pagerState.currentPage,
@@ -41,30 +45,49 @@ fun SimpleTabSwitch(
             containerColor = Color.Transparent,
             divider = { },
             indicator = { tabPositions ->
-                TabRowDefaults.PrimaryIndicator(
-                    Modifier
-                        .tabIndicatorOffset(tabPositions[pagerState.currentPage]),
-                    color = colorPrimary,
-                    shape = RoundedCornerShape(100.dp),
-                )
+                /*  TabRowDefaults.PrimaryIndicator(
+                      Modifier
+                          .tabIndicatorOffset(tabPositions[pagerState.currentPage]),
+                      color = colorPrimary,
+                      shape = RoundedCornerShape(100.dp),
+                  )*/
             }
         ) {
             tabsTitle.forEachIndexed { index, title ->
                 Tab(
                     text = {
                         Text(
-                            title,
-                            fontSize = 18.sp,
+                            modifier = Modifier
+                                //  .defaultMinSize(minWidth = 20.dp, minHeight = 1.dp)
+                                .drawBehind {
+                                    drawRoundRect(
+                                        color = if (pagerState.currentPage == index) colorPrimary else Color.Transparent,
+                                        cornerRadius = CornerRadius(100f)
+                                    )
+                                }
+                                .padding(horizontal = 16.dp, vertical = 8.dp)
+                                .clickable(
+                                    interactionSource = interactionSource,
+                                    indication = null,
+                                    enabled = true,
+                                    onClick = {
+                                        coroutineScope.launch {
+                                            pagerState.animateScrollToPage(index)
+                                        }
+                                    }
+                                ),
+
+                            text = title,
+                            fontSize = 14.sp,
                             maxLines = 1,
                             overflow = TextOverflow.Ellipsis,
-                            color = if (pagerState.currentPage == index) colorPrimary else textGrey
+                            color = if (pagerState.currentPage == index) Color.White else textGrey
                         )
+
                     },
                     selected = pagerState.currentPage == index,
                     onClick = {
-                        coroutineScope.launch {
-                            pagerState.animateScrollToPage(index)
-                        }
+
                     }
                 )
             }
